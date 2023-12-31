c601c1f9e0209139d72e6529f77d5b64e86df54e
Julien <julienauvray28@gmail.com>
2024-01-01 00:51:19 +0100

    fix: update create method for matching with Client change

--- src/core/useCases/client/create.test.js
+++ src/core/useCases/client/create.test.js
@@ -1,107 +1,102 @@
-import { describe, it } from 'node:test'
-import assert from 'node:assert'
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
-import CreateClient from './create.js'
-import InMemoryClientRepository from '../../../adapters/repositories/inMemoryClientRepository.js'
+import CreateClient from "./create.js";
+import InMemoryClientRepository from "../../../adapters/repositories/inMemoryClientRepository.js";
+import Client from "../../entities/client.js";
 
-describe('Create Client', () => {
-    it('should create a new client', async () => {
-        //Arrange
-        const inMemoryClientRepository = new InMemoryClientRepository()
-        const createClient = new CreateClient(inMemoryClientRepository)
+describe("Create Client", () => {
+  it("should create a new client", async () => {
+    //Arrange
+    const inMemoryClientRepository = new InMemoryClientRepository();
+    const createClient = new CreateClient(inMemoryClientRepository);
 
-        //Act
-        await createClient.execute({
-            firstName: 'John',
-            lastName: 'Doe',
-        })
+    //Act
+    await createClient.execute({
+      firstName: "John",
+      lastName: "Doe",
+    });
 
+    // Assert
+    const clients = await inMemoryClientRepository.getClients();
+    assert.strictEqual(clients.length, 1);
+  });
+
+  it("should create a namesake client", async () => {
+    //Arrange
+    const inMemoryClientRepository = new InMemoryClientRepository();
+    const createClient = new CreateClient(inMemoryClientRepository);
+
+    await createClient.execute({
+      firstName: "John",
+      lastName: "Doe",
+    });
+
+    //Act
+    await createClient.execute({
+      firstName: "John",
+      lastName: "Doe",
+    });
+
+    // Assert
+    const clients = await inMemoryClientRepository.getClients();
+    const firstExpectedClient = Client.create("John", "Doe");
+    firstExpectedClient.id = 1;
+    const secondExpectedClient = Client.create("John", "Doe");
+    secondExpectedClient.id = 2;
+
+    assert.strictEqual(clients.length, 2);
+    assert.deepStrictEqual(clients[0], firstExpectedClient);
+    assert.deepStrictEqual(clients[1], secondExpectedClient);
+  });
+
+  const testCases = [
+    {
+      case: "empty last name",
+      payload: {
+        firstName: "John",
+        lastName: "",
+      },
+      expected: "First and last name are required",
+    },
+    {
+      case: "empty first name",
+      payload: {
+        firstName: "",
+        lastName: "Doe",
+      },
+      expected: "First and last name are required",
+    },
+    {
+      case: "empty first and last name",
+      payload: {
+        firstName: "",
+        lastName: "",
+      },
+      expected: "First and last name are required",
+    },
+    {
+      case: "missing last name",
+      payload: {
+        firstName: "John",
+      },
+      expected: "First and last name are required",
+    },
+  ];
+
+  for (const testCase of testCases) {
+    it(`when the client have ${testCase.case}, should return an error ${testCase.expected}`, async () => {
+      //Arrange
+      const inMemoryClientRepository = new InMemoryClientRepository();
+      const createClient = new CreateClient(inMemoryClientRepository);
+
+      //Act
+      try {
+        await createClient.execute(testCase.payload);
+      } catch (error) {
         // Assert
-        const clients = await inMemoryClientRepository.getClients();
-        assert.strictEqual(clients.length, 1);
-    })
-
-    it('should create a namesake client', async () => {
-        //Arrange
-        const inMemoryClientRepository = new InMemoryClientRepository()
-        const createClient = new CreateClient(inMemoryClientRepository)
-
-        await createClient.execute({
-            firstName: 'John',
-            lastName: 'Doe',
-        })
-
-        //Act
-        await createClient.execute({
-            firstName: 'John',
-            lastName: 'Doe',
-        })
-
-        // Assert
-        const clients = await inMemoryClientRepository.getClients();
-
-        assert.strictEqual(clients.length, 2);
-        assert.deepStrictEqual(clients, [
-            {
-                id: 1,
-                firstName: 'John',
-                lastName: 'Doe',
-            },
-            {
-                id: 2,
-                firstName: 'John',
-                lastName: 'Doe',
-            },
-        ]);
-    })
-
-    const testCases = [
-        {
-            case: 'empty last name',
-            payload: {
-                firstName: 'John',
-                lastName: '',
-            },
-            expected: "Invalid last name",
-        },
-        {
-            case: 'empty first name',
-            payload: {
-                firstName: '',
-                lastName: 'Doe',
-            },
-            expected: "Invalid first name",
-        },
-        {
-            case: 'empty first and last name',
-            payload: {
-                firstName: '',
-                lastName: '',
-            },
-            expected: "Invalid first name and last name",
-        },
-        {
-            case: 'missing last name',
-            payload: {
-                firstName: 'John',
-            },
-            expected: "Invalid last name",
-        },
-    ]
-
-    for (const testCase of testCases) {
-        it(`when the client have ${testCase.case}, should return an error ${testCase.expected}`, async () => {
-            //Arrange
-            const inMemoryClientRepository = new InMemoryClientRepository()
-            const createClient = new CreateClient(inMemoryClientRepository)
-
-            //Act
-            try {
-                await createClient.execute(testCase.payload)
-            } catch (error) {
-                // Assert
-                assert.strictEqual(error.message, testCase.expected)
-            }
-        })
-    }
-})
+        assert.strictEqual(error.message, testCase.expected);
+      }
+    });
+  }
+});
