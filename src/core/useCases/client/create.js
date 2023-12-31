831dc962e3b3c870ad976d14c8d6fc7aeb3e4740
Julien <julienauvray28@gmail.com>
2024-01-01 00:51:19 +0100

    fix: update create method for matching with Client change

--- src/core/useCases/client/create.js
+++ src/core/useCases/client/create.js
@@ -1,14 +1,14 @@
 import Client from "../../entities/client.js";
 
 class CreateClient {
-    constructor(clientRepository) {
-        this.clientRepository = clientRepository;
-    }
+  constructor(clientRepository) {
+    this.clientRepository = clientRepository;
+  }
 
-    async execute(client) {
-        const newClient = Client.createClient(client.firstName, client.lastName);
-        await this.clientRepository.save(newClient);
-    }
+  async execute(client) {
+    const newClient = Client.create(client.firstName, client.lastName);
+    await this.clientRepository.save(newClient);
+  }
 }
 
 export default CreateClient;
