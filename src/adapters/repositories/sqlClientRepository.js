bec30681d9577fc85e3095de94f0de02145064b5
Julien <julienauvray28@gmail.com>
2024-01-08 21:46:38 +0100

    fix: improve docker config improve testContainers spli dump into separate files

--- src/adapters/repositories/sqlClientRepository.js
+++ src/adapters/repositories/sqlClientRepository.js
@@ -1,45 +1,11 @@
-import getConnection from "../../config/database.js";
 import Client from "../../core/entities/client.js";
 import ClientRepository from "../../core/ports/clientRepository.js";
 import pinoLogger from "../loggers/pino.js";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
 class SqlClientRepository extends ClientRepository {
-  constructor() {
+  constructor(connection) {
     super();
-  }
-
-  async init() {
-    this.connection = await getConnection();
-    await this.loadDumpFile();
-  }
-
-  async loadDumpFile() {
-    pinoLogger.info("Loading dump file");
-    try {
-      const __dirname = path.dirname(fileURLToPath(import.meta.url));
-      const dumpPath = path.join(__dirname, "../../config/dump.sql");
-      const dump = fs.readFileSync(dumpPath, "utf8");
-      const queries = dump.split(";");
-
-      for (const query of queries) {
-        const trimmedQuery = query.trim();
-        if (
-          trimmedQuery !== "" &&
-          !trimmedQuery.startsWith("/*!") &&
-          !trimmedQuery.endsWith("*/")
-        ) {
-          await this.connection.query(trimmedQuery);
-        }
-      }
-
-      pinoLogger.info("SQL dump imported successfully");
-    } catch (error) {
-      pinoLogger.info("Error importing SQL dump:");
-      pinoLogger.error(error);
-    }
+    this.connection = connection;
   }
 
   async getClients() {
