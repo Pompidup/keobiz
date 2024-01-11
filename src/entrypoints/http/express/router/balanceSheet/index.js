0c534db6be6596e3edcb5a9e4813b8fbf208a03e
Julien <julienauvray28@gmail.com>
2024-01-11 11:03:52 +0100

    feat: add swagger docs update Readme

--- src/entrypoints/http/express/router/balanceSheet/index.js
+++ src/entrypoints/http/express/router/balanceSheet/index.js
@@ -3,6 +3,22 @@ import container from "../../../../di/container.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/balance_sheet/{clientId}:
+ *   get:
+ *     summary: Retrieve a balance sheet by clientId.
+ *     parameters:
+ *       - in: path
+ *         name: clientId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric ID of the client to retrieve.
+ *     responses:
+ *       200:
+ *         description: Balance sheets attach to a client.
+ */
 router.get("/balance_sheet/:clientId", async (req, res, next) => {
   const findBS = container.resolve("findBS");
   const { clientId } = req.params;
@@ -15,6 +31,33 @@ router.get("/balance_sheet/:clientId", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/balance_sheet:
+ *   post:
+ *     summary: Create a new balance sheet
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               clientId:
+ *                 type: integer
+ *                 description: The ID of the client
+ *                 required: true
+ *               year:
+ *                 type: integer
+ *                 description: The year of the balance sheet
+ *                 required: true
+ *               result:
+ *                 type: integer
+ *                 description: The result of the balance sheet
+ *                 required: true
+ *     responses:
+ *       201: {}
+ */
 router.post("/balance_sheet", async (req, res, next) => {
   const createBalanceSheet = container.resolve("createBS");
   const { year, clientId, result } = req.body;
@@ -31,6 +74,36 @@ router.post("/balance_sheet", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/balance_sheet/{clientId}/{year}:
+ *   patch:
+ *     summary: Update a balance sheet by clientId and year
+ *     parameters:
+ *       - in: path
+ *         name: clientId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: year
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               result:
+ *                 type: integer
+ *                 description: The new result of the balance sheet
+ *                 required: true
+ *     responses:
+ *       201: {}
+ */
 router.patch("/balance_sheet/:clientId/:year", async (req, res, next) => {
   const updateBalanceSheet = container.resolve("updateBS");
   const { clientId, year } = req.params;
@@ -48,6 +121,25 @@ router.patch("/balance_sheet/:clientId/:year", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/balance_sheet/{clientId}/{year}:
+ *   delete:
+ *     summary: Delete a balance sheet by clientId and year
+ *     parameters:
+ *       - in: path
+ *         name: clientId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: year
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204: {}
+ */
 router.delete("/balance_sheet/:clientId/:year", async (req, res, next) => {
   const deleteBalanceSheet = container.resolve("deleteBS");
   const { clientId, year } = req.params;
